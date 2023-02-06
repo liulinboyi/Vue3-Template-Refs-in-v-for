45441776c25bbf97b23f549b5388832925b69f10
liulinboyi <814921718@qq.com>
2023-02-06 21:16:29 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Product from './components/Product.vue';
+import { ref } from 'vue';
+
+let products = ref([]);
+
+let items = ['abc', 'def'];
+
+const show = () => {
+  products.value.forEach((product: any) => {
+    console.log(product.externalId);
+  });
+};
+</script>
+
+<template>
+  <Product
+    v-for="item in items"
+    :externalId="item"
+    :key="item"
+    ref="products"
+  />
+
+  <button @click="show">Test</button>
+</template>
---
+++ src/components/Product.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+interface Props {
+  externalId: string;
+}
+
+const { externalId } = defineProps<Props>();
+
+defineExpose({
+  externalId,
+});
+</script>
+<template>
+  <div>Item: {{ externalId }}</div>
+</template>
